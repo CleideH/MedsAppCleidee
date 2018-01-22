6dced22730a76180f510e5af193d75cb2e992b23
CleideH <cleideh@github.com>
2018-01-22 17:01:29 -0200

    Implementation Profile e Medicines

--- app/src/main/java/br/ufop/cleide/MainActivity.java
+++ app/src/main/java/br/ufop/cleide/MainActivity.java
@@ -1,14 +1,50 @@
 package br.ufop.cleide;
 
+import android.content.Intent;
+import android.icu.util.ValueIterator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void call(View view){
+
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void medicines(View view){
+
+        Intent it = new Intent(this,MedicinesList.class);
+        startActivity(it);
+
+    }
+
+    public void profile(View view){
+
+        Intent it = new Intent(this,ProfileEdit.class);
+        startActivity(it);
+
+    }
+
+    public void doctors(View view){
+
+        Intent it = new Intent(this,DoctorList.class);
+        startActivity(it);
+
     }
 
 
